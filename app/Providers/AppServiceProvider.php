79e57dd6e573fe8f4966b2b556ec14db9e366d96
Luiz <luizh_17@hotmail.com>
2022-03-30 16:02:36 -0300

    Cadastro, validação e tratamento dos dados de usuário

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace LaraDev\Providers;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        Blade::component('admin.components.message', 'message');
     }
 
     /**
